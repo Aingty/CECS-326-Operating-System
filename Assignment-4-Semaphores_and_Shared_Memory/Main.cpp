031a6fa18d21483ad80dea417152c4b9838f5fd6
Aingty Eung <aingtyeung@yahoo.com>
2018-05-06 21:45:23 -0700

    Testing

--- Assignment-4-Semaphores_and_Shared_Memory/Main.cpp
+++ Assignment-4-Semaphores_and_Shared_Memory/Main.cpp
@@ -94,13 +94,13 @@ void calculate(SEMAPHORE &sem, bool *U_Taken, bool *V_Taken)
 		value = U;
 		*U_Taken = true;
 		*currTaken = *U_Taken;
+		cout << "value" << endl;
 	}
 	if(*V_Taken == false)
 	{
 		value = V;
 		*V_Taken = true;
 		*currTaken = *V_Taken;
-		cout << "value" << endl;
 	}
 	//sem.P(mySemaphore);
 	// while(randomGenerator <= 100 || randomGenerator%value == 0)
