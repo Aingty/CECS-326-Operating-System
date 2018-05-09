64c6c0ac8db577fbbad5c7783230304a67a12360
Aingty Eung <aingtyeung@yahoo.com>
2018-05-09 14:04:43 -0700

    Testing

--- Assignment-4-Semaphores_and_Shared_Memory/Main.cpp
+++ Assignment-4-Semaphores_and_Shared_Memory/Main.cpp
@@ -128,7 +128,7 @@ void calculate(SEMAPHORE &sem, char *shmBUF, char childName)
 	do
 	{
 		randomGenerator = rand()%100000 + 1;
-		cout << childName << " Generated: " << randomGenerator <<" Working on "<< value << endl;
+		//cout << childName << " Generated: " << randomGenerator <<" Working on "<< value << endl;
 	}
 	while(randomGenerator >= 100 || value % randomGenerator != 0);
 	cout << "Child: "<< childName << " is done!" << endl;
